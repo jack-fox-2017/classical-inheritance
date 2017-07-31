a9c2c39005055a15ab34ed220dfd89c26e895d8b
alfandash <cumiasem91@gmail.com>
2017-07-31 19:10:50 +0700

    classical inheritance done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,76 @@
 "use strict"
+
+class SuperPower {
+  constructor() {
+
+  }
+
+  use_laser_vision() {
+    return 'Mengluarkan laser. pi pi pi pi'
+  }
+
+  be_invisible(){
+    return 'jurus menghilang ciattt'
+  }
+}
+
+class Animal{
+  constructor(num_legs,is_warm_blooded) {
+    this.num_legs = num_legs
+    this.is_warm_blooded = is_warm_blooded
+    this.superPower = new SuperPower()
+  }
+}
+
+class Frog extends Animal{
+  constructor(num_legs,is_warm_blooded) {
+    super(num_legs,is_warm_blooded)
+  }
+
+  kaki(){
+    return this.num_legs
+  }
+
+  darah(){
+    return this.is_warm_blooded
+  }
+}
+
+
+class Ayam extends Animal {
+  constructor(num_legs,is_warm_blooded) {
+    super(num_legs,is_warm_blooded)
+  }
+
+  kaki(){
+    return this.num_legs
+  }
+
+  darah(){
+    return this.is_warm_blooded
+  }
+}
+
+class Cumi extends Animal{
+  constructor(num_legs,is_warm_blooded) {
+    super(num_legs,is_warm_blooded)
+  }
+
+  kaki(){
+    return this.num_legs
+  }
+
+  darah(){
+    return this.is_warm_blooded
+  }
+}
+
+
+var animal = new Animal(2,true)
+var bangkong = new Frog(4,true)
+var ayam = new Ayam(2,true)
+var cumi = new Cumi(0,false)
+
+console.log(bangkong.kaki());
+console.log(bangkong.superPower.use_laser_vision());
+console.log(ayam.superPower.be_invisible());
