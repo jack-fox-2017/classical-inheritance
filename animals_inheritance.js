e7f7efc8fb8823273925f7e97dd3839df9ccb53c
Renataclara <renataclarak@gmail.com>
2017-07-31 20:06:36 +0700

    done with superpower in class Animal

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,10 +1,23 @@
 "use strict"
+class SuperPower {
+  constructor(nama) {
+    this.nama = nama
+  }
+  use_laser_vision(){
+    return console.log('cut it cut laser by '+this.nama);
+  }
+
+  be_invisible(){
+    return console.log('kamu tidak bisa melihat '+this.nama);
+  }
+}
 
 class Animal {
   constructor(name, num_legs =4, is_warm_blooded =true) {
     this.name =name
     this.num_legs= num_legs
     this.is_warm_blooded = is_warm_blooded
+    this.superpower = new SuperPower(name)
   }
 
 }
--- species.js
+++ species.js
@@ -1,8 +1,9 @@
 const Animal = require('./animals_inheritance')
 
 class Frog extends Animal {
-  constructor(num_legs, bloodWarmness) {
+  constructor(num_legs, bloodWarmness,colour) {
     super('Frog',num_legs, bloodWarmness)// mesti panggil parent
+    this.colour = colour;
   }
 }
 
@@ -21,26 +22,25 @@ class Bat extends Animal {
 class Chimpanzee extends Animal {
   constructor(num_legs, bloodWarmness) {
     super('Chimpanzee',num_legs, bloodWarmness)// mesti panggil parent
-    this.superpower = new SuperPower('Chimpanzee') // COMPOSITION MANGGIL CLASS LAIN DI SESUATU CLASS
+    // this.superpower = new SuperPower('Chimpanzee') // COMPOSITION MANGGIL CLASS LAIN DI SESUATU CLASS
   }
 }
 
-class SuperPower {
-  constructor(nama) {
-    this.nama = nama
-  }
-  use_laser_vision(){
-    return console.log('cut it cut laser by '+this.nama);
-  }
+// class SuperPower {
+//   constructor(nama) {
+//     this.nama = nama
+//   }
+//   use_laser_vision(){
+//     return console.log('cut it cut laser by '+this.nama);
+//   }
+//
+//   be_invisible(){
+//     return console.log('kamu tidak bisa melihat '+this.nama);
+//   }
+// }
 
-  be_invisible(){
-    return console.log('kamu tidak bisa melihat '+this.nama);
-  }
-}
 
-
-let frog = new Frog(undefined, false);
-console.log(frog);
+let frog = new Frog(undefined, false,'green');
+frog.superpower.use_laser_vision();
 let chimpanzee = new Chimpanzee(2, undefined);
-console.log(chimpanzee);
-console.log(chimpanzee.superpower.use_laser_vision());
+chimpanzee.superpower.be_invisible();
