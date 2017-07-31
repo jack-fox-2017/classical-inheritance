520ff101aaea43abb11df1b2be06eae2250e8ccc
Pragistyo Machmud <new.SOS.hope@outlook.com>
2017-07-31 21:53:38 +0700

    tugas inhertance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,59 @@
 "use strict"
+class Animal{
+  constructor(leg = 2, warm_blood,name){
+    this._leg = leg;
+    this._blood = warm_blood
+    this._name = name
+    this._superpower = new SuperPower()
+  }
+}
+
+class Frog extends Animal{
+  constructor(leg, warm_blood, name, type){
+    super(leg, warm_blood, name)
+    this._type = type
+  }
+
+}
+
+class Bat extends Animal{
+  constructor(leg, warm_blood, name, special){
+    super(leg, warm_blood, name)
+    this._special = special
+
+  }
+}
+
+class Chimpanze extends Animal{
+  constructor(leg, warm_blood, name){
+    super(leg, warm_blood, name)
+  }
+}
+
+class Fox extends Animal{
+  constructor(leg, warm_blood,name){
+    super(leg, warm_blood,name)
+  }
+}
+//
+// class Chicken extends Animal{
+//   constructor(leg, warm_blood){
+//     super(leg, warm_blood)
+//   }
+// }
+
+class SuperPower {
+  constructor(){
+
+  }
+  rocket_dash(){
+  return 'Rocket DASH !!'
+  }
+}
+
+let animal = new Fox(4, 'warm', 'fox')
+let unggas = new Bat(2, 'warm', 'Kelelawar', 'flying')
+
+console.log(`${unggas._name}, a ${unggas._special} Expert`  )
+console.log(`${animal._name} is ${animal._leg}`)
+console.log(`${animal._name} ready for ${animal._superpower.rocket_dash()}`);
