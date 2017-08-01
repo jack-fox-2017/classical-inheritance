3f87022adabe5caa58c485f8b492f3080dabec56
hariantara <hariantara.iputu@gmail.com>
2017-08-01 21:20:29 +0700

    inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,54 @@
 "use strict"
+
+class Animal {
+  constructor(kaki) {
+    this.kaki = kaki || 2;
+
+  }
+
+  get foot()
+  {
+    return this.kaki;
+  }
+
+  warnaBulu(color)
+  {
+    console.log(`Binatang ini memiliki ${this.kaki} kaki dan warna bulu ${color}`);
+  }
+}
+
+
+class Anjing extends Animal {
+  constructor(kaki, makanan, age) {
+    super(kaki);
+    this.makanan = makanan;
+    this.power = new Power(age);// composition
+  }
+
+  pelihara()
+  {
+    console.log((`Anjing adalah hewan berkaki ${this.kaki} yang makan ${this.makanan}` ));
+  }
+
+}
+
+class Power {
+  constructor(age) {
+
+    this.age = age;
+  }
+
+  waterBender(power)
+  {
+    console.log(`anjing ini memiliki kekuatan yang bisa mengendalikan ${power} and age is ${this.age}` );
+  }
+
+}
+
+
+//var power = new Power(age)
+
+var binatang = new Anjing(4, 'Daging', 9);
+binatang.warnaBulu('Hitam');
+binatang.pelihara();
+binatang.power.waterBender('air');
